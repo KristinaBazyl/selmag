7990eba7bc00917987561b05c988f08dbafe7f99
Kristina_Bazyl <kristi40890@yandex.ru>
2025-12-13 14:19:38 +0300

    lesson4_2_finally

--- manager-app/src/main/java/ag/selm/manager/controller/ProductsController.java
+++ manager-app/src/main/java/ag/selm/manager/controller/ProductsController.java
@@ -5,9 +5,12 @@ import ag.selm.manager.client.ProductsRestClient;
 import ag.selm.manager.controller.payload.NewProductPayload;
 import ag.selm.manager.entity.Product;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,7 +20,10 @@ public class ProductsController {
     private final ProductsRestClient productsRestClient;
 
     @RequestMapping(value = "list", method = RequestMethod.GET)
-    public String getProductsList(Model model, @RequestParam(name = "filter", required = false) String filter) {
+    public String getProductsList(Model model, @RequestParam(name = "filter", required = false) String filter,
+                                  Principal principal) {
+        LoggerFactory.getLogger(ProductsController.class)
+                        .info("Principal: {} ", principal);
         model.addAttribute("products", this.productsRestClient.findAllProduct(filter));
         model.addAttribute("filter", filter);
         return "catalogue/products/list";
