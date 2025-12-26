ded39680aa46ec72f1bd9c8b9bd2684c4045331c
Kristina_Bazyl <kristi40890@yandex.ru>
2025-12-26 22:47:22 +0300

    lesson8

--- feedback-service/src/main/java/ag/selm/feedback/controller/ProductReviewsRestController.java
+++ feedback-service/src/main/java/ag/selm/feedback/controller/ProductReviewsRestController.java
@@ -6,6 +6,9 @@ import ag.selm.feedback.entity.ProductReview;
 import ag.selm.feedback.service.ProductReviewsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
@@ -17,6 +20,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static org.springframework.data.mongodb.core.query.Query.*;
+
 @RestController
 @RequestMapping("feedback-api/product-reviews")
 @RequiredArgsConstructor
@@ -24,19 +29,26 @@ public class ProductReviewsRestController {
 
     private final ProductReviewsService productReviewsService;
 
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
 
+    //    @GetMapping("by-product-id/{productId:\\d+}")
+//    public Flux<ProductReview> findProductReviewsByProductId(@PathVariable("productId") int productId){
+//        return this.productReviewsService.findProductReviewsByProduct(productId);
+//    }
     @GetMapping("by-product-id/{productId:\\d+}")
-    public Flux<ProductReview> findProductReviewsByProductId(@PathVariable("productId") int productId){
-        return this.productReviewsService.findProductReviewsByProduct(productId);
+    public Flux<ProductReview> findProductReviewsByProductId(@PathVariable("productId") int productId) {
+        return this.reactiveMongoTemplate
+                .find(query(Criteria.where("productId").is(productId)),
+                        ProductReview.class);
     }
 
 
     @PostMapping
     public Mono<ResponseEntity<ProductReview>> createProductReview(
             @Valid @RequestBody Mono<NewProductReviewPayload> payloadMono,
-            UriComponentsBuilder uriComponentsBuilder){
+            UriComponentsBuilder uriComponentsBuilder) {
         return payloadMono
-                .flatMap(payload-> this.productReviewsService.createProductReview(payload.productId(),
+                .flatMap(payload -> this.productReviewsService.createProductReview(payload.productId(),
                         payload.rating(), payload.review()))
                 .map(productReview -> ResponseEntity
                         .created(uriComponentsBuilder.replacePath("/feedback-api/product-reviews/{id}")
--- feedback-service/src/main/java/ag/selm/feedback/repository/FavoriteProductRepository.java
+++ feedback-service/src/main/java/ag/selm/feedback/repository/FavoriteProductRepository.java
@@ -1,17 +1,18 @@
 package ag.selm.feedback.repository;
 
 import ag.selm.feedback.entity.FavouriteProduct;
+import ag.selm.feedback.entity.ProductReview;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface FavoriteProductRepository {
-    
-    Mono<FavouriteProduct> save(FavouriteProduct favouriteProduct);
+import java.util.UUID;
+
+public interface FavoriteProductRepository extends ReactiveCrudRepository<FavouriteProduct, UUID> {
 
     Mono<Void> deleteByProductId(int productId);
 
     Mono<FavouriteProduct> findByProductId(int productId);
 
-    Flux<FavouriteProduct> findAll();
 }
 
